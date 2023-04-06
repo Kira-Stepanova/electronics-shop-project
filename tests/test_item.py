fb1b5e688653a9ac6a7dff24148b13a4e2f821cf
Kira Stepanova <stepanovakira99@gmail.com>
2023-04-06 16:06:20 +0300

    improved methods and added tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,11 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+
+def test_Item_init(test_class):
+    assert test_class.name == "Смартфон"
+    assert test_class.price == 10000
+    assert test_class.quantity == 20
+
+
+def test_calculate_total_price(test_class):
+    assert test_class.calculate_total_price() == 200000
